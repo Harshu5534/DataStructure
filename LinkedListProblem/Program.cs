b47a70655ea3f498180fd49bb0e56060cccc7dc3
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-29 17:53:55 +0530

    [Harshal]Add:Data Added Into Linked List

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -12,7 +12,8 @@ namespace LinkedListProblem
             list.Add(30);
             list.Add(70);
             list.Display();
-
+            list.AddInReverseOrder(70);
+            list.Display();
         }
     }
 }
