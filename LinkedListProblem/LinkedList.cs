76299b670ea3cd9ec7993a237456e6af91fcfc55
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-30 18:22:43 +0530

    [Harshal]Add:Search Element In Linked List

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -70,6 +70,7 @@ namespace LinkedListProblem
                 count++;
             }
             return count;
+            Console.WriteLine("Search Value Is: "+value);
         }
 
         public Node InsertAtParticularPosition(int position, int data)
